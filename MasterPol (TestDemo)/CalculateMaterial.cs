a6314ba45b29dc21d1eeef5a1904908203c349ea
Puzacheva <olga.isfff@gmail.com>
2025-05-26 04:21:54 +0300

    -

---
+++ MasterPol (TestDemo)/CalculateMaterial.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MasterPol__TestDemo_
+{
+    internal class CalculateMaterial
+    {
+        /*private int Calculate(int product_type_id, int material_type_id, int product_quantity, double parameter1, double parameter2)
+        {
+            if (product_quantity <= 0 || 
+        }*/
+    }
+}
